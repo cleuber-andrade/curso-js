79e1f70189a0d76c98ccc974f3337e643202fca0
cleuber-andrade <cleuber_binho@hotmail.com>
2021-09-30 10:08:27 -0300

    update

---
+++ objetos/function_factory_prototypy/function_factory_proto.js
@@ -0,0 +1,37 @@
+
+//composição//mixin
+
+const falar = {
+    falar() {
+        console.log(`${this.nome} está falando`);
+    },
+};
+
+const comer = {
+    comer() {
+        console.log(`${this.nome} está comendo`);
+    },
+};
+
+const beber = {
+    beber() {
+        console.log(`${this.nome} está comendo`);
+    },
+};
+
+const pessoasPrototype = Object.assign({}, falar, comer, beber);
+//const pessoasPrototype = { ...falar, ...comer, ...beber };
+
+function criaPessoa(nome, sobrenome){    
+
+    return Object.create(pessoasPrototype, {
+        nome: {value: nome},
+        sobrenome: {value: sobrenome},
+    });
+}
+
+const p1 = criaPessoa('Cleuber', 'Andrade');
+const p2 = criaPessoa('Nathy', 'Andrade');
+
+console.log(p2);
+
