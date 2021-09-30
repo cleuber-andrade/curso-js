b6e20156fc2b86dfc4a957b49b88e8b8045663e1
cleuber-andrade <cleuber_binho@hotmail.com>
2021-09-30 16:36:16 -0300

    Create heranca_class.js

---
+++ classes/heranca_class/heranca_class.js
@@ -0,0 +1,65 @@
+class DispositivoEletronico {
+    constructor(nome){
+        this.nome = nome,
+        this.ligado = false
+    }
+
+    ligar(){
+        if(this.ligado){
+            console.log(`${this.nome} já está ligado`)
+            return;
+        }
+
+        this.ligado = true;
+    } 
+
+    desligar(){
+        if(!this.ligado){
+            console.log(`${this.nome} já está desligado`)
+            return;
+        }
+
+        this.ligado = false;
+    }
+}
+
+//const d1 = new DispositivoEletronico('Smartphone');
+//d1.ligar();
+//d1.desligar();
+//d1.desligar();
+//console.log(d1);
+
+
+
+
+//criando classe derivada da principal//herda tudo da class DispositivoEletronico
+class Smartphone extends DispositivoEletronico{
+    constructor(nome, cor, modelo) {
+        //SUPER serve para chamar os item dentro do constructor PAI(DispositivoEletronico)
+        super(nome),
+        //acrescentando mais itens no constructor
+        this.cor = cor,
+        this.modelo = modelo
+    }
+}
+
+const s1 = new Smartphone ('Samsung', 'Preto', 'Galaxy S10')
+console.log(s1);
+
+class Tablet extends DispositivoEletronico{
+    constructor (nome, temWifi){
+        super(nome),
+
+        this.temWifi = temWifi
+    }
+
+    //ligar(){
+    //    console.log('Olha, você alterou o método ligar.');
+    //}
+}
+
+
+
+const l1 = new Tablet ('Multilaser', true);
+
+console.log(l1.ligar);
