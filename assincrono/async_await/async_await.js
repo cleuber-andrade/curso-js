910d0946989f37aca4701576941554c96ddd0b15
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-05 18:51:03 -0300

    Update async_await.js

--- assincrono/async_await/async_await.js
+++ assincrono/async_await/async_await.js
@@ -20,5 +20,22 @@ function esperaAi(msg, tempo){
     });
 }
 
-esperaAi('Fase 1', rand());
+async function executa(){
+    try{
+        const fase1 = await esperaAi('Fase 1', rand());
+    console.log(fase1);
 
+    const fase2 = await esperaAi('Fase 2', rand());
+    console.log(fase2);
+
+    const fase3 = await esperaAi('Fase 3', rand());
+    console.log(fase3);
+
+    console.log('Terminamos na fase:', fase3);
+    } 
+    catch (e) {
+        console.log(e);
+    }        
+}
+
+executa();
