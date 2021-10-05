d86b935184c08dc2a7350c25015e49cbf316b2cf
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-05 20:09:58 -0300

    update

--- assincrono/xmlhttprequest_get_promise/assets/js/xmlhttprequest_get_promise.js
+++ assincrono/xmlhttprequest_get_promise/assets/js/xmlhttprequest_get_promise.js
@@ -1,15 +1,44 @@
 const request = obj => {
-    const xhr = new XMLHttpRequest();
-    //get
-    xhr.open(obj.method, obj.url, true);
-    //set
-    xhr.send();
+    return new Promise((resolve, reject) =>{
+        const xhr = new XMLHttpRequest();
+        //get
+        xhr.open(obj.method, obj.url, true);
+        //set
+        xhr.send();
 
-    xhr.addEventListener('load', ()=>{
+        xhr.addEventListener('load', ()=>{
         if(xhr.status >= 200 && xhr.status < 300){
-            obj.success(xhr.responseText);
+            resolve(xhr.responseText);
         }else{
-            obj.error(xhr.statusText);                
+            reject(xhr.statusText);                
         }
-    });
+        });
+    });    
+};
+
+document.addEventListener('click', e =>{
+    const el = e.target;
+    const tag =  el.tagName.toLowerCase();
+
+    if(tag === 'a'){
+        e.preventDefault()
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el){
+    const href = el.getAttribute('href');
+
+    const objConfig = {
+        method: 'GET',
+        url: href        
+    };
+
+    const response = await request(objConfig);
+    carregaResultado(response);
+}
+
+function carregaResultado(response){
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
 }
