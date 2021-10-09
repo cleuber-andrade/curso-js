8868811b2598958075cfeb0df0690a6e887510ac
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-09 18:25:48 -0300

    update

--- js_tooling_ES6_modules/validando_cpf/src/main.js
+++ js_tooling_ES6_modules/validando_cpf/src/main.js
@@ -1,1 +1,9 @@
-import './assets/css/style.css';
+import GeraCPF from '../src/modules/GeraCPF';
+
+import '../src/assets/style.css';
+
+(function(){
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpfGerado');
+    cpfGerado.innerHTML = gera.geraNovoCpf();
+})();
