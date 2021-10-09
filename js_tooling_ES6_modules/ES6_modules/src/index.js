a2e4125e057b553c405fa01f8bfabca0210485be
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-09 14:59:59 -0300

    update

--- js_tooling_ES6_modules/ES6_modules/src/index.js
+++ js_tooling_ES6_modules/ES6_modules/src/index.js
@@ -1,19 +1,3 @@
-/* 
-Este arquivo é apenas um exemplo que adicionei após
-as aulas para garantir que não teremos erros com
-classes, funções assíncronas e outras coisas relacionadas.
-*/
-import { Person } from './test-modules/person';
+import { nome } from './modulo';
 
-const person1 = new Person('Luiz', 'Otávio');
-
-// Teste de promise (2 segundos)
-person1.fetchData().then((_person) => console.log(person1));
-
-// Teste async/await
-async function run() {
-  // 2 segundos
-  const pessoa = await new Person('João', 'Miranda').fetchData();
-  console.log(pessoa.firstName, pessoa.lastName);
-}
-run();
+console.log(nome);
---
+++ js_tooling_ES6_modules/ES6_modules/src/modulo.js
@@ -0,0 +1,9 @@
+const nome = 'Cleuber';
+const sobrenome = 'Andrade';
+const idade = 34;
+
+function soma(x, y){
+    return x + y; 
+}
+
+export { nome };
