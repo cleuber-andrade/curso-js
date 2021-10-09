65b623a5e96bdeb8b4b2d1ccb63371cdb6bedde9
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-09 18:50:05 -0300

    update

--- js_tooling_ES6_modules/validando_cpf/src/modules/validaCPF.js
+++ js_tooling_ES6_modules/validando_cpf/src/modules/validaCPF.js
@@ -44,11 +44,11 @@ export default class ValidaCPF {
     }
   }
   
-  let validacpf = new ValidaCPF('070.987.720-03');
+  //let validacpf = new ValidaCPF('070.987.720-03');
   // validacpf = new ValidaCPF('999.999.999-99');
   
-  if (validacpf.valida()) {
-    console.log('CPF válido');
-  } else {
-    console.log('CPF inválido');
-  }
+  //if (validacpf.valida()) {
+  //  console.log('CPF válido');
+  //} else {
+  //  console.log('CPF inválido');
+  //}
