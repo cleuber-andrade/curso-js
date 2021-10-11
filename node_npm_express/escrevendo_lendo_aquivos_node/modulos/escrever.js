c0367c1c7869ae94d028f827bd22af32c76a035d
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-11 16:06:18 -0300

    update

--- node_npm_express/escrevendo_lendo_aquivos_node/modulos/escrever.js
+++ node_npm_express/escrevendo_lendo_aquivos_node/modulos/escrever.js
@@ -2,4 +2,14 @@ const fs = require('fs').promises;
 const path = require('path');
 const caminhoArquivo = path.resolve(__dirname, '..','text.txt');
 
-fs.writeFile(caminhoArquivo, );
+const pessoas = [
+    {nome: 'joao'},
+    {nome: 'Binho'},
+    {nome: 'Nathy'},
+    {nome: 'Leila'},
+    {nome: 'Isac'}
+];
+
+const json = JSON.stringify(pessoas);
+
+fs.writeFile(caminhoArquivo, json, { flag: 'w'});
