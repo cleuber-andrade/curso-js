855dafd4848c702926e8dbd7dae293ba683b197d
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-11 12:37:15 -0300

    update

--- node_npm_express/modulos2/mod.js
+++ node_npm_express/modulos2/mod.js
@@ -1,5 +1,5 @@
-//navegando das pastas - ./B/C/D/mod = o . significa que é da minha pasta atual para frente.
-//../ é para voltar ../ = significa subir o nível da pasta, ou seja, subir para a pasta acima. 
+//navegando das pastas - (./B/C/D/mod) = o ./ significa que é da minha pasta atual para frente.
+//../ é para voltar - (../../../api_rest) = ../ significa subir o nível da pasta, ou seja, subir para a pasta acima. 
 
 
 //module.exports = (x, y) => x * y;
