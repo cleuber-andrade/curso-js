c98475075394129b25d44ea746a79ca0518ce292
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-11 13:02:42 -0300

    update

--- node_npm_express/npm/npm.js
+++ node_npm_express/npm/npm.js
@@ -1,4 +1,27 @@
 //CD serva para acessar a parta a frente no terminal;
+
 //iniciar o npm - npm init -y
+
 // no package.json - "name": "npm" é literalmente o nome da pasta.
 
+//para instalar (pacotes, frameworks e etc)no npm - npm install express 
+
+
+
+/*
+{
+    "name": "npm", (nome da pasta raiz)
+    "version": "1.0.0",
+    "description": "",
+    "main": "npm.js", (nome do arquivo dentro da pasta raiz)
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "keywords": [],
+    "author": "",
+    "license": "ISC",
+    "dependencies": {  (as instalações ficam aqui em dependencies)
+      "express": "^4.17.1"
+    }
+  }
+*/ 
