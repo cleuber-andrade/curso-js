c33d7b9be21f86ff1fc97d706f3262d14f00c92b
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-11 12:34:34 -0300

    update

---
+++ node_npm_express/modulos2/app.js
@@ -0,0 +1,6 @@
+//const multiplicacao = require('./mod');
+//console.log(multiplicacao(2, 2));
+
+const Cachorro = require('./mod');
+const cachorrinho = new Cachorro('Nenezinho');
+cachorrinho.latir();
---
+++ node_npm_express/modulos2/mod.js
@@ -0,0 +1,15 @@
+//navegando das pastas - ./B/C/D/mod = o . significa que é da minha pasta atual para frente.
+//../ é para voltar ../ = significa subir o nível da pasta, ou seja, subir para a pasta acima. 
+
+
+//module.exports = (x, y) => x * y;
+
+module.exports = class Cachorro {
+    constructor (nome){
+        this.nome = nome;
+    }
+
+    latir(){
+        console.log(`${this.nome} está fazendo au au`);
+    }
+}
