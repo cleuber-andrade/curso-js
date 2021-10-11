b70210fce129be8b603cc73b316e782ed6916ce7
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-11 14:23:48 -0300

    Create atualizar_node.js

---
+++ node_npm_express/npm/atualizar_node.js
@@ -0,0 +1,10 @@
+//#REMOVE O NODE ANTERIOR
+//SUDO = ADMINISTRADOR
+//sudo rm /etc/apt/source.list.d/nodesource.list*
+
+//#INSTALA O CURL
+//sudo apt install curl -y
+
+//#BAIXAR E INSTALAR O NODE MAIS RECENTE
+//curl -sl https://deb.nodesource.com.setup_14.x | sudo bash - 
+//sudo apt install nodejs -y
