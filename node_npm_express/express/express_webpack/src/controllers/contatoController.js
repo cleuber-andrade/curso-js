6b1fc2f4e34f4b00ba1e5e7b0983821284cc1cfa
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-12 14:57:58 -0300

    update

---
+++ node_npm_express/express/express_webpack/src/controllers/contatoController.js
@@ -0,0 +1,7 @@
+exports.paginaInicial = (req, res) =>{
+    res.send(`Obrigador por entrar em contato.`);
+};
+
+exports.tratandoPost = (req, res) => {
+    res.send(`Oi sou a rota do contato`);
+};
