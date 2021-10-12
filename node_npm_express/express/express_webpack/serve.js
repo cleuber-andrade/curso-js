c3134dd474547b9bf7b8b4c76eb5965c5d37debe
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-12 16:49:44 -0300

    Update serve.js

--- node_npm_express/express/express_webpack/serve.js
+++ node_npm_express/express/express_webpack/serve.js
@@ -3,16 +3,22 @@ const app = express();
 const routes = require('./routes');
 const path = require('path');
 
+
 app.use(express.urlencoded({ extended: true }));
-
+//chamando o conteúdo statico(html, css, imagens e etc...)
+//app.use(express.static('./public'));
 app.use(express.static(path.resolve(__dirname, 'public')));
-
+//caminho relativo para a pasta views
+//app.set('views', './src/views');
+//caminho absoluto para a pasta views
 app.set('views', path.resolve(__dirname, 'src', 'views'));
+//selecionando a engine a ser utilizada no projeto
+//para instalar no - npm i ejs
 app.set('view engine', 'ejs');
-
+//chamando as routes.js
 app.use(routes);
 
 app.listen(3000, () => {
-  console.log('Acessar http://localhost:3000');
-  console.log('Servidor executando na porta 3000');
+    console.log('Acessar http://localhost:3000');
+    console.log('Esse servidor está executando na porta 3000');
 });
