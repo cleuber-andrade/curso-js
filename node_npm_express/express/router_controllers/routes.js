d9defa76405821d61bc638194b4f9d2d3d044e7b
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-12 13:40:56 -0300

    update

--- node_npm_express/express/router_controllers/routes.js
+++ node_npm_express/express/router_controllers/routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./controllers/homeController');
+const contatoController = require('./controllers/contatoController');
 
 //rotas da home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeCOntroller.trataPost);
 
-
+//rotas contato
+route.get('/', contatoController.paginaInicial);
+route.post('/', contatoController.tratandoPost);
 
 
 
