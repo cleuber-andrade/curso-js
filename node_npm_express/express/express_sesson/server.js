a005d64a7c29656d0afa2d40b12387858fa95044
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-13 14:55:42 -0300

    update

--- node_npm_express/express/express_sesson/server.js
+++ node_npm_express/express/express_sesson/server.js
@@ -10,15 +10,39 @@ mongoose.connect(process.env.CONNECTIONSTRING)
         app.emit('pronto');
     })
     .catch(e => console.log(e)); 
-    
+
+//chamando a express-session    
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+
+
+
 
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal, outroMiddlewareGlobal } = require('./src/middlewares/middlewares');
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'ajsdfhalsjdhfasdjhlasdjhflas',
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    resave:false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
+
 app.set('view engine', 'ejs');
 
 app.use(middlewareGlobal);
