57d5f28abae94b1a3134d0137c59bf11e354e1e3
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-13 16:45:19 -0300

    update

--- node_npm_express/conteudo_views/src/controllers/homeController.js
+++ node_npm_express/conteudo_views/src/controllers/homeController.js
@@ -1,5 +1,8 @@
-exports.paginaInicial = (req, res) => {
-  res.render('index');
+exports.paginaInicial = (req, res) => {  
+  res.render('index', {
+    titulo: 'Este é o título da página',
+    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  });
   return;
 };
 
--- node_npm_express/conteudo_views/src/middlewares/middlewares.js
+++ node_npm_express/conteudo_views/src/middlewares/middlewares.js
@@ -1,4 +1,5 @@
 exports.middlewareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = 'Este é o valor da varival local';
     next();
   };
   
