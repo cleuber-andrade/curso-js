8ae61b256d719952bd15a9dfeb28cacc1ccf49e0
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-14 12:41:04 -0300

    update

--- node_npm_express/resumo/routes.js
+++ node_npm_express/resumo/routes.js
@@ -3,6 +3,9 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
+//AS ROTAS DEFINEM QUAL O CONTROLLERS VAI CONTROLAR AQUELA ROTA.(QUEM VAI DESCIDIR O QUE VAI ACONTECER NA ROTA).
+//basicamente o trabalho do controller é descidir qual que é o MODEL e VIEWS que será utilizado na rota.
+//NO CASO DOS EXEMPLOS ABAIXOS OS CONTROLLES SÃO O homeController e contatoController.
 // Rotas da home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
--- node_npm_express/resumo/src/models/HomeModel.js
+++ node_npm_express/resumo/src/models/HomeModel.js
@@ -1,3 +1,6 @@
+//O TRABALHO DOS MODELS É TUDO REFERENTE AOS DADOS DA APLICAÇÃO, OU A BASE DE DADOS
+
+
 const mongoose = require('mongoose');
 
 const HomeSchema = new mongoose.Schema({
