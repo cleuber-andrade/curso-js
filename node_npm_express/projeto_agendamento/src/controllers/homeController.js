1d632abcedf0f59c88ef0db463226f39741504a3
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-14 16:45:10 -0300

    update

--- node_npm_express/projeto_agendamento/src/controllers/homeController.js
+++ node_npm_express/projeto_agendamento/src/controllers/homeController.js
@@ -1,12 +1,3 @@
-exports.paginaInicial = (req, res) => {  
-  res.render('index', {
-    titulo: 'Este é o título da página',
-    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  });
-  return;
-};
-
-exports.trataPost = (req, res) => {
-  res.send(req.body);
-  return;
+exports.index = (req, res) => {  
+  res.render('index');
 };
