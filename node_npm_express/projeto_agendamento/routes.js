6e2bdbffe86970bd775eb1e2c9aeb4492990a654
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-15 17:14:50 -0300

    update

--- node_npm_express/projeto_agendamento/routes.js
+++ node_npm_express/projeto_agendamento/routes.js
@@ -3,8 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
-
 const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middlewares');
 
 //'/' tbm pode ser escrito com /index dentro do get
 
@@ -18,7 +19,10 @@ route.post('/login/login/', loginController.login);
 route.get('/login/logout/', loginController.logout);
 
 //rotas de contato
-route.get('contato/index', contatoController.index);
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+
 
 
 module.exports = route;
