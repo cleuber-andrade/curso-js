cbdad184a082b3ecdb69a3c0da3c7632a3ec53c7
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-15 14:07:57 -0300

    update

--- node_npm_express/projeto_agendamento/src/controllers/loginController.js
+++ node_npm_express/projeto_agendamento/src/controllers/loginController.js
@@ -1,5 +1,4 @@
 //instalar o bcryptjs no npm
-
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
@@ -21,6 +20,31 @@ exports.register = async ( req, res ) => {
         }
 
         req.flash('success', 'Seu usuário foi criado com sucesso!');
+        req.session.save(()=>{
+            return res.redirect('/login/');
+        });          
+    }catch(e){
+        console.log(e);
+        return res.render('404');
+    }
+
+};
+
+exports.login = async ( req, res ) => {
+    try{
+        const login = new Login(req.body);
+        await login.login();
+    
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(()=>{
+                return res.redirect('/login/');
+            });
+            return;
+        }       
+
+        req.flash('success', 'Você entrou no sistema.');
+        req.session.user = login.user;
         req.session.save(()=>{
             return res.redirect('/login/');
         });          
--- node_npm_express/projeto_agendamento/src/models/LoginModel.js
+++ node_npm_express/projeto_agendamento/src/models/LoginModel.js
@@ -16,6 +16,24 @@ class Login {
     this.user = null;
   }
 
+  async login(){
+    this.valida();
+    if(this.errors.length > 0) return; 
+    this.user = await LoginModel.findOne({email: this.body.email});
+    //bcrpty validando a senha do usuário
+    if(!this.user){
+      this.errors.push('Usuário não existe!');
+      return;
+    } 
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+      this.errors.push('Senha Inválida');
+      this.user = null;
+      return;
+    }
+
+  }
+
   async register(){
     this.valida();
     if(this.errors.length > 0) return;
@@ -27,17 +45,11 @@ class Login {
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
     
-    try{
-      this.user = await LoginModel.create(this.body);
-    } catch(e){
-      console.log(e);
-    }    
-  }
+    this.user = await LoginModel.create(this.body);
+  }  
 
   async userExists() {
-    const user = await LoginModel.findOne({email: this.body.email});
-    
-    if(user) this.errors.push('Usuário já existe.');
+    this.user = await LoginModel.findOne({email: this.body.email});if(this.user) this.errors.push('Usuário já existe.');
   }
 
   valida(){
