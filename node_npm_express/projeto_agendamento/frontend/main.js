4004e6b90183752220667eb15b36cda1fd35e987
cleuber-andrade <cleuber_binho@hotmail.com>
2021-10-16 14:14:00 -0300

    update

--- node_npm_express/projeto_agendamento/frontend/main.js
+++ node_npm_express/projeto_agendamento/frontend/main.js
@@ -1,6 +1,14 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './assets/css/style.css';
+import Login from './modules/login';
 
-console.log('Olá mundo 3');
+const login = new Login('.form-cadastro');
+const cadastro = new Login('.form-login');
+login.init();
+cadastro.init();
+
+
+//import './assets/css/style.css';
+
+
